5358ae77f4532f2213e471c19a3b5ff2d6653b1a
cs50victor <cs50victor@github.com>
2020-06-07 16:31:32 -0700

    testing api

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,10 @@ import Layout from "../components/Layout";
 import {Link } from "gatsby";
 import "../styles/index.scss";
 
-const client = new ApolloClient({
-  uri: "https://icanhazdadjoke.com/graphql"
-})
-
 export default function Home() {
   return (
     <Layout>
+      <div><h1>WWWWWW</h1></div>
       <Link to="./testing">WTF</Link>
     </Layout>
   )
