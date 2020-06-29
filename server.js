426e7be4c6fd9ab8cd2305e61858f11e4a3d08cc
cs50victor <cs50victor@github.com>
2020-06-29 14:50:18 -0700

    graphl node server

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const {buildSchema} = require("graphql");
 const app = express();
 
 const port = process.env.PORT || 8080;
 
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+    hello: () => {
+      return 'Hello world!';
+    },
+};
 app.use(express.static(__dirname + "/public"));
 
+app.use('/__graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 //routes
 
